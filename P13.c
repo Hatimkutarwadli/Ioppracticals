535a7250eb753117210fedf19c398c82b6516272
Hatim Kutarwadli <62584115+Hatimkutarwadli@users.noreply.github.com>
2024-09-09 22:17:22 +0530

    Add files via upload

---
+++ P13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () { 
+    int n;
+    printf("Enter the Number: \n");
+    scanf("%d", &n);
+    if (n%2==0) {
+        printf("The Number is Even.");
+    }
+    else {
+        printf("The Number is Odd.");
+    }
+}
