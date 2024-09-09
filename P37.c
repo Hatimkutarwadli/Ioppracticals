19da29d7b9dfeef4d32c35a1249a9f879bc2017a
Hatim Kutarwadli <62584115+Hatimkutarwadli@users.noreply.github.com>
2024-09-09 22:13:08 +0530

    Delete P37.c

--- P37.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main () {
-    int n;
-    printf("To Print Even Terms till N! \n");
-    scanf("%d", &n);
-    for (int i = 0; i<=n; i=i+2){
-            printf("%d,", i);
-        }
-}
